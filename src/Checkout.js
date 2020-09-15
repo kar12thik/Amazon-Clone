b5320c87b6e8a3690141666f10455d4e54946eff
Karthikeyan Subramanian <skarthik.chennai@gmail.com>
2020-09-15 22:30:54 +0530

    Modifying checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -15,12 +15,11 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
-
         <div>
           <h3>Hello, {user?.email}</h3>
-          <h2 className="checkout__title">Your shopping Basket</h2>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
 
-          {basket.map(item => (
+          {basket.map((item) => (
             <CheckoutProduct
               id={item.id}
               title={item.title}
@@ -29,7 +28,6 @@ function Checkout() {
               rating={item.rating}
             />
           ))}
-
         </div>
       </div>
 
